37e896da8871a80f6bc2eaa56fffba0ebb8a75b6
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2022-03-17 00:19:17 +0330

    Fix WPAdminRequestRule

--- src/Rules/WPAdminRequestRule.php
+++ src/Rules/WPAdminRequestRule.php
@@ -13,13 +13,13 @@ class WPAdminRequestRule implements IRule
         if (null === $path) {
             return 0;
         }
-        if ('/wp-admin/' !== substr($path, 0, strlen('/wp-admin/'))) {
-            return 0;
+        if ('/wp-admin/' === substr($path, 0, strlen('/wp-admin/'))) {
+            return -1;
         }
-        if ('/wp-json/yoast/' !== substr($path, 0, strlen('/wp-json/yoast/'))) {
-            return 0;
+        if ('/wp-json/yoast/' === substr($path, 0, strlen('/wp-json/yoast/'))) {
+            return -1;
         }
 
-        return -1;
+        return 0;
     }
 }
