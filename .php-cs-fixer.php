97c3bc92f4b0479f34c57ac58abb91f4f5350279
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2022-03-16 22:37:26 +0330

    Disable 'phpdoc_to_comment' rule

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -10,6 +10,7 @@ $finder = PhpCsFixer\Finder::create()
 $config = new PhpCsFixer\Config();
 return $config->setRules([
         '@Symfony' => true,
+        'phpdoc_to_comment' => false,
     ])
     ->setFinder($finder)
 ;
