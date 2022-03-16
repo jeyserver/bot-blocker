60e51685d9a2ece03bdd380b2ea59b9d92ebea18
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2022-03-17 01:30:15 +0330

    Work with sub directories and prevent tricks

--- src/Rules/WPBruteForceRule.php
+++ src/Rules/WPBruteForceRule.php
@@ -9,7 +9,7 @@ class WPBruteForceRule extends BruteForceRule
     public function check(LogEntry $entry): float
     {
         $path = $entry->getPath();
-        if (null === $path or !preg_match("/^\/(?:xmlrpc|wp-login)\.php$/", $path)) {
+        if (null === $path or !preg_match("/\/(?:xmlrpc|wp-login)\.php$/", $path)) {
             return 0;
         }
 
